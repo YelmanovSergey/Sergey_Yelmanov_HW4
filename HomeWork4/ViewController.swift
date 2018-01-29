9cc685e4bc2d43c44f2da536413c0faf6eaa61c2
Sergey Yelmanov <sergey.yelmanov@icloud.com>
2018-01-29 19:03:56 +0200

    initial commit

---
+++ HomeWork4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeWork4
+//
+//  Created by Sergey Yelmanov on 29.01.2018.
+//  Copyright © 2018 Sergey Yelmanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
